9c35b259f4623b8d065a1ef7445dc8d7bc74ec09
Nabin Agrawal <nabinagrawal64@gmail.com>
2025-07-22 11:50:49 +0530

    add clarity

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 
+import clarity from '@microsoft/clarity';
+
+clarity.init("siqnbdhvvs"); 
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
